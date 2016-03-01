99ebf6d7c794a6d56427fdb6781f6c60646445a8
Peter Maidens <p.m.maidens@gmail.com>
2016-02-29 22:28:46 -0700

    Update postWriter tests to work

--- client/app/controllers/postWriter_test.js
+++ client/app/controllers/postWriter_test.js
@@ -1,16 +1,16 @@
 /*eslint-env node, qunit, mocha */
 "use strict";
 
-describe("myApp.view1 module", function() {
+describe("myApp.postWriter module", function() {
 
-    beforeEach(module("myApp.view1"));
+    beforeEach(module("myApp.postWriter"));
 
-    describe("view1 controller", function(){
+    describe("postWriter controller", function(){
 
         it("should ....", inject(function($controller) {
             //spec body
-            var view1Ctrl = $controller("View1Ctrl");
-            expect(view1Ctrl).toBeDefined();
+            var postWriter = $controller("postWriter");
+            expect(postWriter).toBeDefined();
         }));
 
     });
