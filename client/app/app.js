ab26dc455986f9ebc85b7acc5d45bce3eab1ffed
Peter Maidens <p.m.maidens@gmail.com>
2016-03-01 14:24:54 -0700

    Revert "Remove useless version stuff"
    This reverts commit 101c9a3af7a8333d2fe471c2d034fd0f769d84c4.

--- client/app/app.js
+++ client/app/app.js
@@ -5,6 +5,7 @@ angular.module("myApp", [
     "ngRoute",
     "myApp.postWriter",
     "myApp.postStream",
+    "myApp.version"
 ]).
 config(["$routeProvider", function($routeProvider) {
     $routeProvider.otherwise({redirectTo: "/stream"});
