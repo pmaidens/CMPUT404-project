a374472f785b9631c9bc4e0f3f40212747f09ea1
Peter Maidens <p.m.maidens@gmail.com>
2016-02-29 22:20:45 -0700

    Add a place to write posts in view 1

--- client/app/view1/view1.js
+++ client/app/view1/view1.js
@@ -9,6 +9,14 @@ angular.module("myApp.view1", ["ngRoute"])
     });
 }])
 
-.controller("View1Ctrl", [function() {
-
-}]);
+.controller("View1Ctrl", function($scope, $http) {
+    $scope.SubmitPost = function () {
+        var postContent = $scope.postContent;
+        // TODO: Chang this object to whatever it needs to be
+        $http({
+            method: "POST",
+            url: "base/server/url/authors/123/posts",
+            data: postContent
+        });
+    };
+});
