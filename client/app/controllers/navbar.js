82a264b1398d7019fa70a90da01b75954f212b02
Peter Maidens <p.m.maidens@gmail.com>
2016-03-04 12:43:24 -0700

    Remove navbar login call

--- client/app/controllers/navbar.js
+++ client/app/controllers/navbar.js
@@ -4,9 +4,6 @@ angular.module("myApp.navbar", ["myApp.services.authenticationHandler"])
 
 .controller("NavbarController", function($scope, authenticationHandler) {
     $scope.valid = false;
-    $scope.login = function() {
-        authenticationHandler.login("asdf", "asdf");
-    };
     $scope.logout = function() {
         authenticationHandler.logout();
     };
