6dd216b7e4a2edd8b0a9cd77f37745b5126f2ccf
Peter Maidens <p.m.maidens@gmail.com>
2016-03-09 20:46:00 -0700

    More static!

--- BloggingAPI/BloggingAPI/settings.py
+++ BloggingAPI/BloggingAPI/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'BloggingAPI', 'static')
-STATIC_DIRS = os.path.join(BASE_DIR, "static")
+STATIC_DIRS = os.path.join(BASE_DIR, "BloggingAPI/static")
 
 
 REST_FRAMEWORK = {
