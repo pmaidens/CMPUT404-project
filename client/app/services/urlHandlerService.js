f3c6560a8995fe44096dee487300fe13db29c39b
Peter Maidens <p.m.maidens@gmail.com>
2016-03-11 16:51:18 -0700

    Change url to openshift backend

--- client/app/services/urlHandlerService.js
+++ client/app/services/urlHandlerService.js
@@ -3,6 +3,6 @@
 angular.module("myApp.services.urlHandler", [])
 .service("urlHandler", function() {
     this.serviceURL = function () {//eslint-disable-line no-unused-vars
-        return "http://localhost:8080/";
+        return "http://project-c404.rhcloud.com/";
     };
 });
