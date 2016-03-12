e91b23e90c1719503dd7b132dc869e771661b79a
Peter Maidens <p.m.maidens@gmail.com>
2016-03-11 19:57:00 -0700

    Make it go to login if you aren't logged in

--- client/app/services/authenticationHandlerService.js
+++ client/app/services/authenticationHandlerService.js
@@ -4,24 +4,40 @@ angular.module("myApp.services.authenticationHandler", [
     "myApp.services.urlHandler",
     "ngStorage"
 ])
-.service("authenticationHandler", function($q, $http, $localStorage, urlHandler) {
+.service("authenticationHandler", function($q, $http, $localStorage, $rootScope, urlHandler) {
     this.loginWatchers = [];
     this.user = $localStorage.user || {};
     this.token = $localStorage.token || "";
 
+    $rootScope.loggedIn = !!$localStorage.token;
+
     this.login = function (username, password) {
         var url = urlHandler.serviceURL() + "rest-auth/login/";
-        return $http.post(url,{"username":username, "password":password}).then(function(result){
-            this.determineUser(username).then(function () {
-                this.updateWatchers(true);
-            }.bind(this));
-            $http.defaults.headers.common.Authorization = "Token " + result.data.key;
-            this.token = "Token " + result.data.key;
-            $localStorage.token = this.token;
+        return $q(function(resolve, reject) {
+            $http.post(url,{"username":username, "password":password}).then(function(result){
+                this.determineUser(username).then(function () {
+                    this.updateWatchers(true);
+                    resolve(result);
+                }.bind(this));
+                $http.defaults.headers.common.Authorization = "Token " + result.data.key;
+                this.token = "Token " + result.data.key;
+                $localStorage.token = this.token;
+            }.bind(this),function(err){
+                console.log(err);
+                reject(err);
+            });
+        }.bind(this));
+        // return $http.post(url,{"username":username, "password":password}).then(function(result){
+        //     this.determineUser(username).then(function () {
+        //         this.updateWatchers(true);
+        //     }.bind(this));
+        //     $http.defaults.headers.common.Authorization = "Token " + result.data.key;
+        //     this.token = "Token " + result.data.key;
+        //     $localStorage.token = this.token;
 
-        }.bind(this),function(err){
-            console.log(err);
-        });
+        // }.bind(this),function(err){
+        //     console.log(err);
+        // });
     };
 
     this.logout = function() {
@@ -30,7 +46,9 @@ angular.module("myApp.services.authenticationHandler", [
             $http.defaults.headers.common.Authorization = undefined;
             this.token = "";
             delete $localStorage.token;
+            delete $localStorage.user;
             this.user = {};
+            $rootScope.loggedIn = false;
             this.updateWatchers(false);
             resolve();
         }.bind(this));
@@ -49,6 +67,7 @@ angular.module("myApp.services.authenticationHandler", [
             result.data.some(function (author) {
                 if(author.displayname === displayname) {
                     this.user = $localStorage.user = author;
+                    $rootScope.loggedIn = true;
                     return true;
                 }
             }.bind(this));
