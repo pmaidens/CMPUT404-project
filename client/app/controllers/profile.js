1195d0641672859ae2d34c75854a0f88df0d0ee7
BoyanP <bgpeychoff@gmail.com>
2016-03-18 12:14:50 -0600

    added a friend request button

--- client/app/controllers/profile.js
+++ client/app/controllers/profile.js
@@ -24,6 +24,16 @@ angular.module("myApp.profile", [
     $scope.editing = false;
     //TODO: Don't fake this!
     $scope.user = authenticationHandler.user;
+    $scope.canAddFriend = function(){
+
+	if ($scope.postStream.authorId && $scope.user.id != $scope.postStream.authorId){
+
+	    return true;
+	}
+
+	return false; 
+
+    } 
 
     $scope.clickEdit = function () {
         if($scope.user.id === $scope.author.id) {
@@ -38,6 +48,29 @@ angular.module("myApp.profile", [
     };
 
     authorHandler.getAuthor($routeParams.authorId || $scope.user.id).then(function (result) {
+	console.log(result.data);
         $scope.author = result.data;
     });
+
+    $scope.makeFriendReq = function(){
+	//friendReq should actually be made in the back end
+	var friendReq = {
+	    "query":"friendrequest",
+	    "author":{
+		"id": $scope.user.id,
+		"host": $scope.user.host,
+		"displayName": $scope.user.displayname,
+	    },
+	    "friend":{
+		"id":$scope.author.id,
+		"host":$scope.author.host,
+		"displayName": $scope.author.displayname,
+		"url":$scope.author.url
+
+	    }
+
+	};
+
+    }
+
 });
