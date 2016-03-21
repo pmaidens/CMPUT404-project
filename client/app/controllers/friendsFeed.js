3cb2982f9691111c96210eb1b47555b1a5528a77
sjpartri <sjpartri@ualberta.ca>
2016-03-20 21:42:17 -0600

    friendFeed

--- client/app/controllers/friendsFeed.js
+++ client/app/controllers/friendsFeed.js
@@ -14,23 +14,54 @@ angular.module("myApp.friendsFeed", [
     });
 }])
 
-.controller("FriendsFeedController", function($scope, $http, $location, authenticationHandler, urlHandler, authorHandler) {
-	$scope.authors = [];
+.controller("FriendsFeedController", function($scope, $http, $location, $q, authenticationHandler, urlHandler, authorHandler) {
+	$scope.potentialFriends = [];
 	$scope.user = authenticationHandler.user;
 
     authorHandler.getAllAuthors().then(function(result) {
 
+        $scope.potentialFriends = result.data;
+        var followers = $scope.getfollowers();
 
-        $scope.authors = result.data;
+        $q.all([followers, getRefresh($scope.user)]).then(function(){
+            filteredStuff($scope.potentialFriends,$scope.followers,$scope.user,$scope.user.friends)
+        })
+        //var friends = getfriends($scope potentialFriends);
      
     });
 
-    authorHandler.getFollowers($scope.user.id).then(function(result){
+    $scope.getfollowers = function(){
 
-        $scope.followers = result.data;
+    return authorHandler.getFollowers($scope.user.id).then(function(result){
 
+        $scope.followers = result.data[0].friendrequests;
 
-    });
+        });
+
+ 
+
+    };  
+
+    var getRefresh = function(user){
+        return authorHandler.getAuthor(user.id).then(function(result){
+            $scope.followers = result.data.friendrequests;
+            $scope.friends = result.data.friends;
+        });
+    }; 
+    
+    var filteredStuff = function(potentialFriends, followers, user, friends){
+
+     $scope.filteredPotentialFriends = potentialFriends.filter(function(filteredPotentialFriends){
+            console.log(filteredPotentialFriends.id);
+            return (!followers.some(function(follower){
+                return filteredPotentialFriends.id === follower.author_id;
+            })
+            && !friends.some(function(friend){
+                return filteredPotentialFriends.id === friend.author_id;
+            })&& filteredPotentialFriends.id !== user.id);
+        
+            });
+    };
 
     authorHandler.getFollowing($scope.user.id).then(function(result){
 
