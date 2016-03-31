4a251a361a4cb1be338e6013d78b802297b0769e
Peter Maidens <p.m.maidens@gmail.com>
2016-03-31 16:38:38 -0600

    Reformat friendsFeed.js

--- client/app/controllers/friendsFeed.js
+++ client/app/controllers/friendsFeed.js
@@ -15,37 +15,30 @@ angular.module("myApp.friendsFeed", [
 }])
 
 .controller("FriendsFeedController", function($scope, $http, $location, $q, authenticationHandler, urlHandler, authorHandler) {
-	$scope.localAuthors = [];
-	$scope.user = authenticationHandler.user;
+    $scope.localAuthors = [];
+    $scope.user = authenticationHandler.user;
     $scope.isFollowing = false;
     $scope.hasFollowers = false;
     $scope.friends = $scope.user.friends;
-    //console.log($scope.friends.length!=0);
 
     authorHandler.getAllAuthors().then(function(result) {
-
         $scope.localAuthors = result.data;
         var followers = $scope.getfollowers();
 
         $q.all([followers||$scope.followers2, getRefresh($scope.user)]).then(function(){
-            filteredStuff($scope.localAuthors,$scope.followers,$scope.user,$scope.user.friends)
-        })
+            filteredStuff($scope.localAuthors,$scope.followers,$scope.user,$scope.user.friends);
+        });
         //var friends = getfriends($scope localAuthors);
-	//STEP 1 
-	//STEP 2
-	getAuthorsFromNodes(nodes);
-	//console.log($scope.nodeAuthors);
-     
+        //STEP 1
+        //STEP 2
+        getAuthorsFromNodes(nodes);
+        //console.log($scope.nodeAuthors);
     });
 
     $scope.getfollowers2 = function(){
-
-	 authorHandler.getFollowers($scope.user.id).then(function(result){
-
-	     $scope.followers2 = result.data[0].friendrequests;
-	     
-	 });
-
+        authorHandler.getFollowers($scope.user.id).then(function(result){
+            $scope.followers2 = result.data[0].friendrequests;
+        });
     };
     $scope.getfollowers2();
 
@@ -56,9 +49,9 @@ angular.module("myApp.friendsFeed", [
         // 	if($scope.followers.length){
         // 	    $scope.hasFollowers = true;
         // 	}
-	//Step 3
-	//FILTER HERE 
-	//filteredStuff($scope.allAuthors, $scope.followers, authentication.user,authentication.user.friends)
+        //Step 3
+        //FILTER HERE
+        //filteredStuff($scope.allAuthors, $scope.followers, authentication.user,authentication.user.friends)
         // });
         return $q(function(resolve, reject) {
             authorHandler.getFollowers($scope.user.id).then(function(result){
@@ -70,51 +63,40 @@ angular.module("myApp.friendsFeed", [
                 resolve(result);
             });
         });
-    };  
+    };
 
     var getRefresh = function(user){
         return authorHandler.getAuthor(user.id).then(function(result){
             $scope.followers = result.data.friendrequests;
             $scope.friends = result.data.friends;
         });
-    }; 
-    
-    var filteredStuff = function(localAuthors, followers, user, friends){
+    };
 
+    var filteredStuff = function(localAuthors, followers, user, friends){
         $scope.filteredlocalAuthors = localAuthors.filter(function(filteredlocalAuthors){
-           
             return (!followers.some(function(follower){
                 return filteredlocalAuthors.id === follower.author_id;
             })
             && !friends.some(function(friend){
                 return filteredlocalAuthors.id === friend.author_id;
             })&& filteredlocalAuthors.id !== user.id);
-        
         });
     };
 
     authorHandler.getFollowing($scope.user.id).then(function(result){
         $scope.friendsSOON = result.data[0].following;
-	if($scope.friendsSOON.length){
-
-	    $scope.isFollowing = true;
-	}
-	
-
-    
+        if($scope.friendsSOON.length){
+            $scope.isFollowing = true;
+        }
     });
 
-
-
     $scope.makeFriendReq = function(author){
-	
         var friend = {
             "id": author.id,
             "host":author.host,
             "displayName": author.displayName,
             "url": author.url || author.host
         };
-
         var requestObject = {
             query: "friendrequest",
             author:  {
@@ -127,136 +109,48 @@ angular.module("myApp.friendsFeed", [
         };
 
         authorHandler.postFriendRequest(friend).then(function() {
-	    
-        $http.defaults.headers.common.Authorization = authenticationHandler.token;
-	    $http.post(urlHandler.serviceURL() + 'api/addfollower/',requestObject);
-	    alert("Friend Request Sent")}, function() {alert("uh-oh, something went wrong")});
+            $http.defaults.headers.common.Authorization = authenticationHandler.token;
+            $http.post(urlHandler.serviceURL() + "api/addfollower/",requestObject);
+            alert("Friend Request Sent");
+        }, function() {
+            alert("uh-oh, something went wrong");
+        });
     };
-
-   
-
-
     $scope.unfriend = function(friend){
-
-	authorHandler.unfriend(friend).then(function(result){
-
-	    //success!
-	    alert('unfriended!');
-	    $window.location.reload();
-
-
-	});
-
+        authorHandler.unfriend(friend).then(function(){
+            alert("unfriended!");
+        });
     };
-    
+
     $scope.unfollow = function(following){
-
-	authorHandler.unfollow(following).then(function(result){
-
-	    //success!
-	    alert('unfollowed!');
-	    $window.location.reload();
-	});
-
+        authorHandler.unfollow(following).then(function(){
+            alert("unfollowed!");
+        });
     };
 
     $scope.acceptFriend = function(follower){
-	
-	authorHandler.acceptFriend(follower).then(function(result){
-
-		
-	    alert('friend accepted!');
-	    $window.location.reload();
-	
-
-	});
-
+        authorHandler.acceptFriend(follower).then(function(){
+            alert("friend accepted!");
+        });
     };
 
-    var nodes = [{'url':'http://mighty-cliffs-82717.herokuapp.com/api/','username':'Team6','password':'Team6'},{'url':'http://cmput404-team-4b.herokuapp.com/api/' , 'username': 'team6', 'password':'team6' }];
+    var nodes = [{"url":"http://mighty-cliffs-82717.herokuapp.com/api/","username":"Team6","password":"Team6"},{"url":"http://cmput404-team-4b.herokuapp.com/api/" , "username": "team6", "password":"team6" }];
 
-
-  
     var getAuthorsFromNodes = function(nodes){
-
-    $scope.nodeAuthors = [];
-    var encoded='';
-    
-
-
-	for (var i=0; i < nodes.length ; i++){
-	    //console.log(nodes);
-	    
-	    //TODO CHANGE encoded SO THAT IT MATCHES WHAT EACH GROUP WANTS.
-	  //  if (nodes[i].url =='http://cmput404team4b.herokuapp.com/api/'){
-
-
-
-     //   encoded = window.btoa('team6@' + nodes[i].username + ':' + nodes[i].password);
-
-
-	//    }else{
-      //      console.log(nodes[i].url);
-	//	encoded = window.btoa(nodes[i].username + ':' + nodes[i].password);
-
-
-
-     //   }
-      //  $http.defaults.headers.common.Authorization = 'Basic ' + encoded; 
-     //   $http.defaults.useXDomain=true;
-     //   $http({
-
-     /*   method:'GET',
-        url: nodes[i].url+'author/',
-        headers:{
-            
-
+        $scope.nodeAuthors = [];
+        var encoded="";
+        for (var i=0; i < nodes.length ; i++){
+            encoded = window.btoa(nodes[i].username + ":" + nodes[i].password);
+            $http.defaults.headers.common.Authorization = "Basic " + encoded;
+            $http.defaults.useXDomain=true;
+            $http({
+                method:"GET",
+                url: ~nodes[i].url.indexOf("mighty-cliffs-82717") ? nodes[i].url+"authors/" : nodes[i].url+"author/",
+                headers:{
+                }
+            }).then(function(result){
+                $scope.nodeAuthors = $scope.nodeAuthors.concat(result.data);
+            });
         }
-        
-
-        }).then(function(result){
-
-    */
-//    console.log(nodes[i].url);
-//     console.log("here");
-    encoded = window.btoa(nodes[i].username + ':' + nodes[i].password);
-    $http.defaults.headers.common.Authorization = 'Basic ' + encoded; 
-    $http.defaults.useXDomain=true;
-  //  if (nodes[i].url == 'http://cmput404-team-4b.herokuapp.com/api/'){
-// console.log("here2");
-    $http({
-
-        method:'GET',
-        url: ~nodes[i].url.indexOf("mighty-cliffs-82717") ? nodes[i].url+'authors/' : nodes[i].url+'author/',
-        headers:{
-            
-
-        }
-        
-
-        }).then(function(result){
-
-    
-
-        console.log("here3");
-        //TODO 
-       // if(nodes[i].url == 'http://secret-inlet-51780.herokuapp.com/api/'){
-        $scope.nodeAuthors = $scope.nodeAuthors.concat(result.data);
-       // console.log("here444"+$scope.nodeAuthors);
-        //}
-        //$scope.nodeAuthors = result.data;
-        //STEP2
-
-     //   });
-    
-
-
-    });
-//}
-
-}
-
-};
-
+    };
 });
-
